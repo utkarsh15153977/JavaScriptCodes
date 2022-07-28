0b8cfc3a4455abf90a7f44a8735cd8bb65e59fca
utkarsh15153977 <60875408+utkarsh15153977@users.noreply.github.com>
2022-07-28 18:12:47 +0530

    Create Object_Datatype.js

---
+++ Object_Datatype.js
@@ -0,0 +1,14 @@
+/*Object DataTypes are the datatypes which contains multiple values
+Examples 
+1 Array
+2 String
+3 Functions
+*/
+
+//Declering the array
+let car={
+    name:"Duggatti",
+    color:"Red",
+    price:1585500
+}
+console.log(car);
