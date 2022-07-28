7bba2052791c7b9e5545b26e294005b862c1dd68
utkarsh15153977 <60875408+utkarsh15153977@users.noreply.github.com>
2022-07-28 15:27:06 +0530

    Create Operators.js

---
+++ Operators.js
@@ -0,0 +1,38 @@
+var a=10,b=20;
+var c=a+b;
+console.log(c);
+
+var x=3**2
+console.log(x);
+
+/*
+Equal(==)
+int ct=3;
+if(ct==3){
+    ct(bool)=true;
+}
+else{
+    ct(bool)=false;
+}
+Number == String ->True
+
+*/
+
+var ca=3==3;
+console.log(ca);
+
+/*
+Strictly Equal(===)
+Equal operator is not able to differentiate between string and number 
+Strictly Equal is able to differentiate between string and number
+Number===String -> False 
+*/
+//Equal Operator
+var cb=3=='3';
+console.log(cb);
+
+//Strictly Equal(===)
+var cc=5==='5';
+console.log(cc);
+
+
