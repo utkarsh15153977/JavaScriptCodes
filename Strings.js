664466cdeca73131ea4b486452a42975a24737e9
utkarsh15153977 <60875408+utkarsh15153977@users.noreply.github.com>
2022-07-28 17:31:43 +0530

    Create Strings.js

---
+++ Strings.js
@@ -0,0 +1,17 @@
+let s1=123+"Utkarsh";
+document.write(s1);
+
+let s2="Utkarsh "+"Singh";
+document.write(s2);
+
+// Conversion of String to Number
+let s3="12354";
+Number(s3);
+document.write(s3);
+
+//conversion of Number to String 
+//with the help of toString method
+
+let s4=47562;
+s4.toString(s4);
+document.write(s4);
